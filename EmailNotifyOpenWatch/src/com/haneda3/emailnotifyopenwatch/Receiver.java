55fc7f6f56b30921e6a7d697378ea5ac7a481e67
haneda3 <aceplus33@gmail.com>
2011-05-16 05:54:22 +0900

    the first

---
+++ EmailNotifyOpenWatch/src/com/haneda3/emailnotifyopenwatch/Receiver.java
@@ -0,0 +1,15 @@
+package com.haneda3.emailnotifyopenwatch;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Receiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent it = new Intent("com.smartmadsoft.openwatch.action.ENVELOPE");
+        context.sendBroadcast(it);
+	}
+
+}
